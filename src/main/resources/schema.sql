03e14c9555a87d15516e19e144a73394e78c1ee3
mmy <mmy_xize@163.com>
2019-08-31 09:41:52 +0800

    删去parkingRela文件

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,11 @@
 CREATE TABLE parkingBoy (
   employeeID  VARCHAR(64) PRIMARY KEY NOT NULL,
-  name VARCHAR(64) NOT NULL
+  name VARCHAR(64) NOT NULL,
+  parkingLotId varchar(64)
 );
 
 CREATE TABLE parkingLot (
   parkingLotID  VARCHAR(64) PRIMARY KEY NOT NULL,
   capacity INTEGER NOT NULL,
   availablePositionCount INT NOT NULL
-);
-
-CREATE TABLE parkingRela (
-  employeeID  VARCHAR(64) NOT NULL,
-  parkingLotID  VARCHAR(64) NOT NULL
 );
