53e1562b67e53b9bddc6adee4946cb9948678488
mmy <mmy_xize@163.com>
2019-08-31 09:47:27 +0800

    修改数据库：parkingLot table中添加parkingBoyId

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE parkingBoy (
   employeeID  VARCHAR(64) PRIMARY KEY NOT NULL,
-  name VARCHAR(64) NOT NULL,
-  parkingLotId varchar(64)
+  name VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE parkingLot (
   parkingLotID  VARCHAR(64) PRIMARY KEY NOT NULL,
   capacity INTEGER NOT NULL,
-  availablePositionCount INT NOT NULL
+  availablePositionCount INT,
+  parkingBoyId varchar(64) not null
 );
